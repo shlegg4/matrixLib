923f2028445178f350f130a53978d664adde3096
Samuel <samuellegg2004@icloud.com>
2021-11-29 21:05:01 +0000

    made matrix multiply static method

--- Sources/matrixLib/matrixLib.swift
+++ Sources/matrixLib/matrixLib.swift
@@ -49,31 +49,36 @@ public struct Matrix{
             self.data[(row*self.columns) + column] = newValue
         }
     }
+    
+    
+    /// Multiply two matrices together using a naive multiplication algorithm
+    /// - Parameters:
+    ///   - matA: rows : M         columns : K
+    ///   - matB: rows : K         columns : P
+    /// - Returns: Outputs matrix with dimensions M by P
+    public static func multiply(matA : Matrix,matB : Matrix) -> Matrix{
+        var result = Matrix(rows: matA.rows,columns: matB.columns,type: .zeroMatrix)
+        if(matA.columns == matB.rows){
+        for i in 0..<matA.rows{
+            for j in 0..<matB.columns{
+                var sum : Double = 0
+                for k in 0..<matA.columns{
+                    sum += matA[i,k] * matB[k,j]
+                }
+                result[i,j] = sum
+            }
+        }
+        }else{
+            print("dimension error")
+        }
+        return result
+    }
+    
+    
 }
 
 //    matrix operations
-/// Multiply two matrices together using a naive multiplication algorithm
-/// - Parameters:
-///   - matA: rows : M         columns : K
-///   - matB: rows : K         columns : P
-/// - Returns: Outputs matrix with dimensions M by P
-public func multiply(matA : Matrix,matB : Matrix) -> Matrix{
-    var result = Matrix(rows: matA.rows,columns: matB.columns,type: .zeroMatrix)
-    if(matA.columns == matB.rows){
-    for i in 0..<matA.rows{
-        for j in 0..<matB.columns{
-            var sum : Double = 0
-            for k in 0..<matA.columns{
-                sum += matA[i,k] * matB[k,j]
-            }
-            result[i,j] = sum
-        }
-    }
-    }else{
-        print("dimension error")
-    }
-    return result
-}
+
 
 /// Add two matrices together position does not matter function is commutative. Matrices must have same dimensions.
 /// - Parameters:
@@ -111,3 +116,6 @@ public func sub(matA : Matrix,matB : Matrix) -> Matrix{
     }
     return result
 }
+
+
+
